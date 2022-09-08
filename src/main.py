b8aaeedcd88871920ddf6708eedd17e2a9e76100
Wesleyongs <72511481+Wesleyongs@users.noreply.github.com>
2022-09-08 18:07:20 +0800

    Added FastAPI router

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ import datetime as dt
 from typing import List
 
 import uvicorn
-from fastapi import Depends, FastAPI
+from fastapi import Depends, FastAPI, APIRouter
 from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session
 
@@ -32,15 +32,15 @@ def get_db():
     finally:
         db.close()
 
-@app.post("/UrlConversion/", response_model=schemas.UrlConversion)
+@app.post("/UrlConversion/", response_model=schemas.UrlConversion, tags=['UrlConversion'])
 def UrlConversion(input_url, db: Session = Depends(get_db)):
     return crud.create_url_conversion(db=db, input_url=input_url)
 
-@app.get("/UrlConversion/", response_model=List[schemas.UrlConversion])
+@app.get("/UrlConversion/", response_model=List[schemas.UrlConversion], tags=['UrlConversion'])
 def get_converted_url(db: Session = Depends(get_db)):
     return crud.get_url_conversions(db)
 
-@app.delete("/UrlConversion/", response_model=str)
+@app.delete("/UrlConversion/", response_model=str, tags=['UrlConversion'])
 def delete_all_url_conversions(db: Session = Depends(get_db)):
     return crud.delete_all_url_conversions(db)
     
