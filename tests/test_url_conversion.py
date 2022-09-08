34443b95e753798866c47655dc00452c535a3ed6
Wesleyongs <72511481+Wesleyongs@users.noreply.github.com>
2022-09-08 18:45:54 +0800

    Added testcase for creating UrlConversion

--- tests/test_url_conversion.py
+++ tests/test_url_conversion.py
@@ -5,21 +5,18 @@ from fastapi.testclient import TestClient
 # Import local modules
 from src.main import app
 
-client = TestClient(app)
 
-def create_url_conversion():
-    
-    expected_ret_val = [
-        {
+def test_create_url_conversion():
+    client = TestClient(app)
+    expected_ret_val = {
         "input_url": "https://www.askpython.com/python/examples/url-shortener",
-        "output_url": "https://tinyurl.com/y82prysb"
-        }
-    ]
+        "output_url": "https://tinyurl.com/y82prysb",
+    }
 
-    response = client.post("/UrlConversion", headers={"input_url": "https://www.askpython.com/python/examples/url-shortener"})
+    response = client.post(
+        "/UrlConversion/",
+        params={"input_url": "https://www.askpython.com/python/examples/url-shortener"},
+    )
     assert response.status_code == 200
+    print(response.json())
     assert response.json() == expected_ret_val
-    
-    
-    
-    
